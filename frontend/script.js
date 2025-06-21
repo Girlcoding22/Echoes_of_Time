3f89c6c8382c0d4a6c7cd2f2a031449143dcbf43
Girlcoding22 <31711391+Girlcoding22@users.noreply.github.com>
2025-06-21 15:31:29 -0700

    idea 1

---
+++ frontend/script.js
@@ -0,0 +1,4 @@
+// function savePrompt(){
+//             localStorage.setItem('prompt', document.getElementById('prompt').value);
+//             window.location.href = 'generated.html';
+//         }
